bda1900184cf82096b9f9405ed349a2abff0695b
rjm <robjmass@gmail.com>
2024-04-05 19:16:32 -0400

    unnamed commit

--- lua/nvim-gpush/config.lua
+++ lua/nvim-gpush/config.lua
@@ -1,16 +1,17 @@
 local M = {}
 
-local defaults = {
+--local defaults = {
+M.defaults = {
     auto_gpush_on_write = false,
     one_liner = true
 }
 
-M.options = {}
+--M.options = {}
 
-function M.setup(options)
-    M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
-end
+--function M.setup(options)
+--    M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
+--end
 
-M.setup()
+--M.setup()
 
 return M
