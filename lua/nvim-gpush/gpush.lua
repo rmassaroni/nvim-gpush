f236021f5e8e93a3a79f0d3912d6d7d8db4bd85a
rjm <robjmass@gmail.com>
2024-04-05 17:37:42 -0400

    bash test exit code

--- lua/nvim-gpush/gpush.lua
+++ lua/nvim-gpush/gpush.lua
@@ -2,11 +2,14 @@ local M = {}
 
 function M.gpush()
     print("gpushing")
-    local result = os.execute("echo hello")
+    local result, exit_code = os.execute("echo hello")
     if result == true then
         print("success")
     else
         print("fail")
+        if exit_code then
+            print("exit_code: ", exit_code)
+        end
     end
 end
 
