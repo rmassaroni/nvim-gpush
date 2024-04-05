c7d83cd0c3e72f536969723a973b38eca8028b6a
rjm <robjmass@gmail.com>
2024-04-05 17:32:32 -0400

    bash test

--- lua/nvim-gpush/gpush.lua
+++ lua/nvim-gpush/gpush.lua
@@ -2,6 +2,12 @@ local M = {}
 
 function M.gpush()
     print("gpushing")
+    local result = os.execute("bash -c '" .. "echo hello" .. "' executed successfully.")
+    if result == true then
+        print("success")
+    else
+        print("fail")
+    end
 end
 
 return M
