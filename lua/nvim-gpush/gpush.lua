e73f12d96103fcf2e2253136f2d9ffd2e35af383
rjm <robjmass@gmail.com>
2024-04-05 17:43:46 -0400

    bash test success

--- lua/nvim-gpush/gpush.lua
+++ lua/nvim-gpush/gpush.lua
@@ -2,14 +2,19 @@ local M = {}
 
 function M.gpush()
     print("gpushing")
-    local result, exit_code = os.execute("echo hello")
-    if result == true then
+    --local result, exit_code = os.execute("echo hello")
+    local cmd = "wsl echo hello"
+    local handle = io.popen(cmd)
+    local result = handle:read("*a")
+    local exit_code = {handle:close()}
+    if exit_code[1] == true then
         print("success")
     else
         print("fail")
-        if exit_code then
-            print("exit_code: ", exit_code)
+        if exit_code[3] then
+            print("Exit code:", exit_code[3])
         end
+        print("Output:", result)
     end
 end
 
