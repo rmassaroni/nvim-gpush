208edbaa1643555edd5c8dbaa197e6a16216fa5e
rjm <robjmass@gmail.com>
2024-04-07 14:02:28 -0400

    bug fix test

--- lua/nvim-gpush/gpush.lua
+++ lua/nvim-gpush/gpush.lua
@@ -30,7 +30,7 @@ function M.gpush(commit_message)
         --print("success")
         --print("output:", result)
         --print("result" .. result)
-        vim.api.nvim_command('echom result')
+        vim.api.nvim_command('echom "' .. result .. '"')
     else
         print("fail")
         if exit_code[3] then
