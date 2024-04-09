975aa94db5f6012cc39a7bd48a157a8536152e65
rjm <robjmass@gmail.com>
2024-04-09 12:32:34 -0400

    update fix

--- lua/nvim-gpush/gpush_update.lua
+++ lua/nvim-gpush/gpush_update.lua
@@ -5,9 +5,9 @@ local M = {}
 function M.update()
     print("Updating gpush")
     local install_command = "bash <(curl -sSL https://raw.githubusercontent.com/rmassaroni/gpush/install.sh)"
-    local install_result = vim.fn.system(install_command)
+    --local install_result = vim.fn.system(install_command)
 
-    if install_result == 0 then
+    --if install_result == 0 then
         -- Installation successful, check if gpush is now available
         local gpush_updated = vim.fn.system('command -v gpush >/dev/null') == 0
         if gpush_updated then
@@ -15,9 +15,9 @@ function M.update()
         else
             print("Failed to install gpush. Please install it manually.")
         end
-    else
-        print("Failed to install gpush.")
-    end
+    --else
+        --print("Failed to install gpush.")
+    --end
 
 end
 
