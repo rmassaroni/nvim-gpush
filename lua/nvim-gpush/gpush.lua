d1818fe466ee642f28b47d2ef9d3dbeeb89de1c0
rjm <robjmass@gmail.com>
2024-04-10 14:06:32 -0400

    bug fix test

--- lua/nvim-gpush/gpush.lua
+++ lua/nvim-gpush/gpush.lua
@@ -19,8 +19,9 @@ function M.gpush(commit_message)
 
 
     if config.options.debug_mode == true then
-        print(commit_message)
-        print("bash -c 'source " .. script_path .. " && gpush \"" .. tags .. "\"'")
+        print("commit_message: " .. commit_message)
+        print("branch: " .. branch)
+        print("bash -c 'source " .. script_path .. " && gpush " .. tags .. "'")
     end
 
 
