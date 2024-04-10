eaa7aa3f4234a73e833f98acea1ae732e87a22dc
rjm <robjmass@gmail.com>
2024-04-10 14:10:25 -0400

    bug fix test

--- lua/nvim-gpush/gpush.lua
+++ lua/nvim-gpush/gpush.lua
@@ -19,6 +19,7 @@ function M.gpush(commit_message)
 
 
     if config.options.debug_mode == true then
+    
         print("commit_message: " .. commit_message)
         print("branch: " .. branch)
         print("tags: " .. tags)
